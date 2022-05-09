1fdaac4203a3a999fbf7a72c3b8f85fdec4f469e
beer2002 <uvarlin77@gmail.com>
2022-05-09 16:12:03 +0300

    Without Viewer

--- main.cpp
+++ main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include "TText.h"
 #include <conio.h>
-#include "Viewer.h"
+#//include "Viewer.h"
 
 int main() {
     try {
         TText text;
-        //char ch;
 
         text.readFile("../text.txt");
-
-        textProcessor(text);
+        //textProcessor(text);
     } catch (std::exception& exception) {
         std::cerr << exception.what();
     }
 
+    char ch = getch();
     return 0;
 }
